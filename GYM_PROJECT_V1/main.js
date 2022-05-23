2cce837007b634f0295a61f8b8edf551f9c55a86
Matheus-Rodrigues-Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2022-05-23 17:25:19 -0300

    Update main.js

--- GYM_PROJECT_V1/main.js
+++ GYM_PROJECT_V1/main.js
@@ -4,10 +4,10 @@ const menuItems = document.getElementById("menu-items")
 const headerEl = document.getElementById("header-el")
 const bannerEl = document.getElementById("banner-el")
 const memberLink = document.querySelector('.member-link')
+// const courses = document.querySelector(".courses")
 
 
 menuIcon.addEventListener('click', function(){
  navigation.firstElementChild.classList.toggle('show-menu')
  menuItems.classList.toggle('close-menu')
 })
-
