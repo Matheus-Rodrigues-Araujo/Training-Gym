fa45cbf273f102f279248b86bfef87953c28bece
Matheus-Rodrigues-Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2022-05-26 16:46:26 -0300

    Update main.js

--- GYM_PROJECT_V1/main.js
+++ GYM_PROJECT_V1/main.js
@@ -1,13 +1,64 @@
+const headerEl = document.querySelector("header")
+const main = document.querySelector("main")
+const section = document.querySelector(".section")
+
 const menuIcon = document.getElementById("menu-icon")
 const navigation = document.getElementById("navigation")
 const menuItems = document.getElementById("menu-items")
-const headerEl = document.getElementById("header-el")
+
+const pngImage = document.querySelector('.png-image')
+
+
+// const headerEl = document.getElementById("header-el")
 const bannerEl = document.getElementById("banner-el")
 const memberLink = document.querySelector('.member-link')
+
+const headerText = document.querySelector(".header-text")
 // const courses = document.querySelector(".courses")
 
 
-menuIcon.addEventListener('click', function(){
- navigation.firstElementChild.classList.toggle('show-menu')
- menuItems.classList.toggle('close-menu')
+menuIcon.addEventListener('click', () => {
+//  Adiciona ou remove a classe show-menu dependendo se está
+// ou não na classList(Lista de classes)
+  document.querySelector("header").classList.toggle('show-menu')
+  menuItems.classList.toggle('close-menu')
+
+ // Remove o conteúdo principal
+ if (section.style.display === "block"){
+   section.style.display = "none"
+ }
+ else if(section.style.display ==='none'){
+   section.style.display = 'block'
+ }
 })
+
+// Checking if the window has a scrollY >= 524
+window.addEventListener('scroll', () => {
+  if( scrollY >= 524){
+    headerEl.style.position = 'fixed'
+    headerEl.style.backgroundColor = 'purple'
+  }
+  else{
+    headerEl.style.position = 'absolute'
+    headerEl.style.backgroundColor = 'rgb(0, 0, 0)'
+  }
+})
+
+// function findWindowPosition(){
+//   if( scrollY >= 524){
+//     headerEl.style.position = 'fixed'
+//   }
+//   else{
+//     headerEl.style.position = 'absolute'
+//   }
+// }
+
+function toggleMenuClicked(element){
+  if(element.classList.contains('close-menu')){
+    document.body.style.backgroundColor = "#1b1a1a"
+    main.style.opacity = '0'
+    headerEl.style.position = 'fixed'
+  }
+}
+
+toggleMenuClicked(menuItems)
